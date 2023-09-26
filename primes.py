bd2b3228e9bf83e19d58801ccf266bf3184a015f
Muhammad Islam <muhammad.islam@kcl.ac.uk>
2023-09-26 20:31:13 +0100

    Make primes function that returns a list containing the first number_of_primes prime numbers.

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    num = 2
+    while count < number_of_primes:
+        #num = number_of_primes[count]
+        prime = True
+        for i in range (num-1, 1, -1):
+            if num % i == 0:
+                prime = False
+                break
+        if prime == True:
+            list.append(num)
+            count += 1
+        num += 1
     return list
+
+print(primes(10))
